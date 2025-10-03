2cbcbdc30492510d2e3653dcdf7a3002df3eca33
Danielle Smith <code@danini.dev>
2025-10-03 09:15:40 +0200

    async attempt

--- rustyplug/src/lib.rs
+++ rustyplug/src/lib.rs
@@ -1,27 +1,45 @@
-use crate::danini::wasmplug::host;
+use crate::{
+    danini::wasmplug::logging,
+    exports::danini::wasmplug::{lorem, plugin},
+};
 
 wit_bindgen::generate!({
     path: "../wasmplug/wit",
-    world: "foo"
+    world: "foo",
+    features: ["async"],
+    async: [
+        "export:danini:wasmplug/plugin#init",
+        "export:danini:wasmplug/lorem#generate"
+    ]
 });
 
 struct Component;
 
 export!(Component);
 
-impl Guest for Component {
-    fn init() -> () {
+impl plugin::Guest for Component {
+    async fn init() {
+        let log = logging::Logger::new(logging::Level::Warn);
+        log.log(logging::Level::Debug, "initializing...");
         println!("Init Rustyplug! 🦀");
-
-        let thing = host::Thing {
-            name: "Rustything 9000".into(),
-            foo: 9000,
-        };
-
-        host::do_the_thing(&thing);
     }
 
     fn get_name() -> _rt::String {
         "Rustyplug".into()
+    }
+}
+
+impl lorem::Guest for Component {
+    async fn generate(
+        paras: Option<u32>,
+        sentences: Option<u32>,
+        options: Option<_rt::Vec<_rt::String>>,
+    ) -> Result<_rt::String, _rt::String> {
+        let log = logging::Logger::new(logging::Level::Warn);
+        log.log(logging::Level::Debug, "generating...");
+        Ok(format!(
+            "TODO: paras: {:?}, sentences: {:?}, options: {:?}",
+            paras, sentences, options
+        ))
     }
 }
--- wasmplug/src/main.rs
+++ wasmplug/src/main.rs
@@ -1,60 +1,98 @@
 use std::{
-    collections::HashMap,
     ffi::OsStr,
     fs,
     path::{Path, PathBuf},
 };
 
 use wasmtime::{
-    Engine, Store,
-    component::{Component, HasSelf, Linker, bindgen},
+    Config, Engine, Store,
+    component::{Component, HasSelf, Linker, Resource, bindgen},
 };
 use wasmtime_wasi::{ResourceTable, WasiCtx, WasiCtxView, WasiView};
 
-use crate::danini::wasmplug::host::{Host, Thing};
+use crate::danini::wasmplug::logging;
 
-bindgen!("foo");
+bindgen!({
+    world: "foo",
+    exports: {
+        "danini:wasmplug/plugin/init": async,
+        "danini:wasmplug/lorem/generate": async,
+    },
+    imports: { default: async | trappable },
+    with: {
+        "danini:wasmplug/logging/logger": FooLogger
+    }
+});
 
 pub type PluginId = usize;
 
-pub struct FooState {
-    pub next_plugin_id: PluginId,
-    pub plugin_descs: HashMap<PluginId, PluginDesc>,
+pub struct FooLogger {
+    pub max_level: logging::Level,
 }
 
-impl FooState {
-    pub fn new() -> Self {
-        Self {
-            next_plugin_id: 1,
-            plugin_descs: Default::default(),
-        }
-    }
-
-    pub fn next_id(&mut self) -> PluginId {
-        let id = self.next_plugin_id;
-        self.next_plugin_id += 1;
-        id
-    }
-}
-
+#[derive(Default)]
 pub struct PluginState {
     pub ctx: WasiCtx,
     pub table: ResourceTable,
-    pub plugin_id: PluginId,
-}
-
-impl Host for PluginState {
-    fn do_the_thing(&mut self, t: Thing) {
-        println!("Doing the thing: {}!", t.name);
-    }
 }
 
 impl WasiView for PluginState {
-    fn ctx(&mut self) -> wasmtime_wasi::WasiCtxView<'_> {
+    fn ctx(&mut self) -> WasiCtxView<'_> {
         WasiCtxView {
             ctx: &mut self.ctx,
             table: &mut self.table,
         }
+    }
+}
+
+impl logging::Host for PluginState {}
+
+impl logging::HostLogger for PluginState {
+    async fn new(&mut self, max_level: logging::Level) -> wasmtime::Result<Resource<FooLogger>> {
+        let id = self.table.push(FooLogger { max_level })?;
+        Ok(id)
+    }
+
+    async fn get_max_level(
+        &mut self,
+        logger: Resource<FooLogger>,
+    ) -> wasmtime::Result<logging::Level> {
+        debug_assert!(!logger.owned());
+        let logger = self.table.get(&logger)?;
+        Ok(logger.max_level)
+    }
+
+    async fn set_max_level(
+        &mut self,
+        logger: Resource<FooLogger>,
+        level: logging::Level,
+    ) -> wasmtime::Result<()> {
+        debug_assert!(!logger.owned());
+        let logger = self.table.get_mut(&logger)?;
+        logger.max_level = level;
+        Ok(())
+    }
+
+    async fn log(
+        &mut self,
+        logger: Resource<FooLogger>,
+        level: logging::Level,
+        msg: String,
+    ) -> wasmtime::Result<()> {
+        debug_assert!(!logger.owned());
+        let logger = self.table.get_mut(&logger)?;
+        if (level as u32) <= (logger.max_level as u32) {
+            println!("{msg}");
+        }
+        Ok(())
+    }
+
+    async fn drop(&mut self, logger: Resource<FooLogger>) -> wasmtime::Result<()> {
+        debug_assert!(logger.owned());
+        let _logger: FooLogger = self.table.delete(logger)?;
+        // ... custom destruction logic here if necessary, otherwise
+        // a `Drop for MyLogger` would also work.
+        Ok(())
     }
 }
 
@@ -63,46 +101,47 @@ pub struct PluginDesc {
     pub path: PathBuf,
 }
 
-pub fn load_plugin(
-    state: &mut FooState,
+pub async fn load_plugin(
     engine: &Engine,
     linker: &Linker<PluginState>,
     path: PathBuf,
 ) -> wasmtime::Result<()> {
     let component = Component::from_file(engine, &path)?;
 
-    let plugin_id = state.next_id();
-
-    let plugin_name = {
-        let mut store = Store::new(
-            engine,
-            PluginState {
-                plugin_id,
-                ctx: WasiCtx::default(),
-                table: ResourceTable::new(),
-            },
-        );
-        let plugin = Foo::instantiate(&mut store, &component, linker)?;
-        plugin.call_init(&mut store)?;
-        plugin.call_get_name(&mut store)?
-    };
-
-    state.plugin_descs.insert(
-        plugin_id,
-        PluginDesc {
-            name: plugin_name,
-            path,
+    let mut store = Store::new(
+        engine,
+        PluginState {
+            ctx: Default::default(),
+            table: ResourceTable::new(),
         },
     );
+    let plugin = Foo::instantiate_async(&mut store, &component, linker).await?;
+    let plugin_int = plugin.danini_wasmplug_plugin();
+    plugin_int.call_init(&mut store).await?;
+    let name = plugin_int.call_get_name(&mut store)?;
+
+    println!("Running plugin {}...", name);
+
+    let lorem_int = plugin.danini_wasmplug_lorem();
+
+    let lorem = lorem_int
+        .call_generate(&mut store, Some(3), None, None)
+        .await?;
+
+    println!("=====");
+    println!("{}", lorem.unwrap_or_else(|e| e.to_string()));
+    println!("=====");
 
     Ok(())
 }
 
-pub fn load_plugins(state: &mut FooState, plugins_dir: &Path) -> wasmtime::Result<()> {
-    let engine = Engine::default();
+pub async fn load_plugins(plugins_dir: &Path) -> wasmtime::Result<()> {
+    let mut config = Config::new();
+    config.async_support(true);
+    let engine = Engine::new(&config)?;
     let mut linker: Linker<PluginState> = Linker::new(&engine);
 
-    wasmtime_wasi::p2::add_to_linker_sync(&mut linker)?;
+    wasmtime_wasi::p2::add_to_linker_async(&mut linker)?;
     Foo::add_to_linker::<_, HasSelf<_>>(&mut linker, |state| state)?;
 
     if !plugins_dir.is_dir() {
@@ -112,15 +151,18 @@ pub fn load_plugins(state: &mut FooState, plugins_dir: &Path) -> wasmtime::Resul
     for entry in fs::read_dir(plugins_dir)? {
         let path = entry?.path();
         if path.is_file() && path.extension().and_then(OsStr::to_str) == Some("wasm") {
-            load_plugin(state, &engine, &linker, path)?;
+            load_plugin(&engine, &linker, path).await?;
         }
     }
 
     Ok(())
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> wasmtime::Result<()> {
     println!("Hello, world!");
-    let mut state = FooState::new();
-    load_plugins(&mut state, &Path::new("plugins")).expect("Failed to load plugins!");
+
+    load_plugins(&Path::new("plugins")).await?;
+
+    Ok(())
 }
